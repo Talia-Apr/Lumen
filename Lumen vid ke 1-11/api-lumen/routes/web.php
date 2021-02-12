0f3f5c9a608813833edf508d7a95dcb3f59b0a40
Talia-Apr <taliaaprianti46@gmail.com>
2021-02-12 11:56:51 +0700

    Tugas Minggu ke 6 SMT 4

---
+++ Lumen vid ke 1-11/api-lumen/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api'], function () use ($router){
+
+    //Kategori
+    $router->get('kategori', ['uses' => 'KategoriController@index']);
+
+    $router->get('kategori/{id}', ['uses' => 'KategoriController@show']);
+
+    $router->delete('kategori/{id}', ['uses' => 'KategoriController@destroy']);
+
+    $router->put('kategori/{id}', ['uses' => 'KategoriController@update']);
+
+    $router->post('kategori', ['uses' => 'KategoriController@create']);
+
+    //Pelanggan
+    $router->get('pelanggan', ['uses' => 'PelangganController@index']);
+
+    $router->get('pelanggan/{id}', ['uses' => 'PelangganController@show']);
+
+    $router->delete('pelanggan/{id}', ['uses' => 'PelangganController@destroy']);
+
+    $router->put('pelanggan/{id}', ['uses' => 'PelangganController@update']);
+
+    $router->post('pelanggan', ['uses' => 'PelangganController@create']);
+
+});
+
+
+
+
